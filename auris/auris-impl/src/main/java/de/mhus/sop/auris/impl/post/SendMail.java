010f70ff63fe31e9e8a9920dbcab00708da2c69e
Mike Hummel <mh@mhus.de>
2015-11-19 21:41:54 +0100

    save

--- auris/auris-impl/src/main/java/de/mhus/sop/auris/impl/post/SendMail.java
+++ auris/auris-impl/src/main/java/de/mhus/sop/auris/impl/post/SendMail.java
@@ -3,6 +3,8 @@ package de.mhus.sop.auris.impl.post;
 import java.util.Map;
 import java.util.Properties;
 
+import javax.mail.Message;
+
 import de.mhus.osgi.mail.core.MailUtil;
 import de.mhus.sop.auris.api.AurisPostProcessor;
 import de.mhus.sop.auris.api.model.LogEntry;
@@ -22,8 +24,8 @@ public class SendMail extends AurisPostProcessor {
 		to = config.getString("to", null);
 		subject = config.getString("subject","Auris Log");
 		Properties properties = new Properties();
-		
-		MailUtil.addSmtpIfNotExists("mailQueue", properties);
+//		Message msg;
+//		MailUtil.getSendQueueManager().sendMail(msg, to);
 	}
 
 	@Override
