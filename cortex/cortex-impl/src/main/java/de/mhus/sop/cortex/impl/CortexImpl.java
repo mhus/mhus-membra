45948240c81e644b0cfddc7fafd8715e6989cd3a
Mike Hummel <mh@mhus.de>
2015-11-21 10:08:25 +0100

    rename two to nimbus
    The final name

--- cortex/cortex-impl/src/main/java/de/mhus/sop/cortex/impl/CortexImpl.java
+++ cortex/cortex-impl/src/main/java/de/mhus/sop/cortex/impl/CortexImpl.java
@@ -1,8 +1,12 @@
 package de.mhus.sop.cortex.impl;
 
+import java.util.UUID;
+
 import de.mhus.lib.core.MLog;
 import de.mhus.sop.cortex.api.CortexApi;
+import de.mhus.sop.cortex.api.CortexSpaceHandlerService;
 import de.mhus.sop.cortex.api.model.CortexProject;
+import de.mhus.sop.cortex.api.model.CortexSpace;
 
 public class CortexImpl extends MLog implements CortexApi {
 
@@ -14,6 +18,24 @@ public class CortexImpl extends MLog implements CortexApi {
 
 	@Override
 	public CortexProject getProject(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CortexSpaceHandlerService getCortexSpaceHandler(String handler) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CortexSpace getSpace(UUID id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CortexSpace getSpace(String shortcut) {
 		// TODO Auto-generated method stub
 		return null;
 	}
