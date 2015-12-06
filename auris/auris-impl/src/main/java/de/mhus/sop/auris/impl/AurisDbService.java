b284d96bf715b44bc511b5886c3d20e5930e3725
Mike Hummel <mh@mhus.de>
2015-12-07 00:14:42 +0100

    move to lib 3.3

--- auris/auris-impl/src/main/java/de/mhus/sop/auris/impl/AurisDbService.java
+++ auris/auris-impl/src/main/java/de/mhus/sop/auris/impl/AurisDbService.java
@@ -6,7 +6,7 @@ import org.osgi.service.component.ComponentContext;
 
 import de.mhus.lib.adb.DbSchema;
 import de.mhus.lib.adb.Persistable;
-import de.mhus.lib.core.configupdater.ConfigString;
+import de.mhus.lib.core.cfg.CfgString;
 import de.mhus.lib.karaf.adb.DbManagerService;
 import de.mhus.lib.karaf.adb.DbManagerServiceImpl;
 import de.mhus.sop.auris.api.model.LogConnectorConf;
@@ -21,7 +21,7 @@ import aQute.bnd.annotation.component.Deactivate;
 @Component(provide=DbManagerService.class,name="AurisDb",immediate=true)
 public class AurisDbService extends DbManagerServiceImpl {
 
-	private final ConfigString dataSourceName = new ConfigString(this, "dataSource", "db_auris");
+	private final CfgString dataSourceName = new CfgString(this, "dataSource", "db_auris");
 	
 	@Activate
 	public void doActivate(ComponentContext ctx) {
